ba42020c3b505cec0113eac19a4f1045ec366e74
hindustantech <kumarrupees505@gmail.com>
2025-11-07 15:55:58 +0530

    add

--- src/controllers/Dashboard.js
+++ src/controllers/Dashboard.js
@@ -264,6 +264,7 @@ function formatTime(dateString) {
     return date.toLocaleTimeString('en-IN', {
         hour: '2-digit',
         minute: '2-digit',
-        hour12: true
+        hour12: true,
+        timeZone: 'Asia/Kolkata'
     });
 }
