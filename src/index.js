580eddf309eac6d300fedd958f220ed1b257006f
hindustantech <kumarrupees505@gmail.com>
2026-04-07 13:11:31 +0530

    add

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ import companyLeadRoutes from "./routes/companyLeadRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set("trust proxy", 1);
+
 app.use(helmet());
 app.use(cors());
 app.use(morgan("combined", { stream: { write: (message) => logger.info(message.trim()) } }));
